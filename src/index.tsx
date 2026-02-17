d9359924695729e5f7e1106774099ae255459071
Jeny Berezhna <JenySmails24@gmail.com>
2026-02-17 19:42:12 +0000

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { AppProvider } from './shared/context/AppContext';
-import './styles/main.scss';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

a7c7267d0f7a48241280d649d9198a7c51952d13
Jeny Berezhna <JenySmails24@gmail.com>
2026-05-24 21:26:42 +0100

    add task solution

--- src/modules/HomePage/components/ProductCard/ProductCard.tsx
+++ src/modules/HomePage/components/ProductCard/ProductCard.tsx
@@ -2,8 +2,8 @@ import { useCart } from '../../../../shared/context/CartContext';
 import { useFavorites } from '../../../../shared/context/FavoritesContext';
 import { Product } from '../../../../types/Product';
 import styles from './ProductCard.module.scss';
-import FavoriteFilled from 'img/icons/FavoriteFilled.svg';
-import FavoriteEmpty from '.img/icons/FavoriteEmpty.svg';
+import FavoriteFilled from '/img/icons/FavoriteFilled.svg';
+import FavoriteEmpty from '/img/icons/FavoriteEmpty.svg';
 
 type Props = {
   product: Product;

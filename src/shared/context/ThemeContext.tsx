87af7c1a7e287d4e3c4c1f7286d7d738195f2951
Jeny Berezhna <JenySmails24@gmail.com>
2026-09-19 21:22:10 +0100

    add task solution

--- src/shared/context/ThemeContext.tsx
+++ src/shared/context/ThemeContext.tsx
@@ -18,7 +18,7 @@ type ThemeContextType = {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<Theme>('light');
+  const [theme, setTheme] = useState<Theme>('dark');
 
   useEffect(() => {
     const saved = localStorage.getItem('theme') as Theme | null;
